65a4416611f68194aae4006a58c00654f5dd6236
Degra02 <filippodegrandi02@gmail.com>
2022-08-14 21:42:42 +0200

    Added .gitignore

--- Loss.py
+++ Loss.py
@@ -14,7 +14,7 @@ class Loss:
 class CategoricalCrossEntropyLoss(Loss):
     def forward(self, y_pred, y_true):
         samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)  # To avoid division by 0
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[range(samples), y_true]
         elif len(y_true.shape) == 2:
